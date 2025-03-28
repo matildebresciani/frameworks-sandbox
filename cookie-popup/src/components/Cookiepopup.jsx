0e8c1cfa96da4f8de4289272c50410766485045f
matilde <mabr0005@stud.kea.dk>
2025-03-28 13:21:30 +0100

    use state popup

--- cookie-popup/src/components/Cookiepopup.jsx
+++ cookie-popup/src/components/Cookiepopup.jsx
@@ -1,20 +1,27 @@
+"use client";
+import { useState } from "react";
+
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { RxCross2 } from "react-icons/rx";
 
 const CookiePopUp = () => {
+    const [cookiesAccepted, setCookiesAccepted] = useState(false);
     return ( 
-        <div className="cookie-popup">
+        !cookiesAccepted && (
+            <div className="cookie-popup">
             <div className="icons">
                 <LiaCookieBiteSolid style={{ fontSize: "32px" }}/>
                 <RxCross2 style={{ fontSize: "15px", cursor: "pointer" }}/>
             </div>
             <div className="cookie-info">
             <p>We use cookies to improve your user experience.</p>
-            <button>I like Cookies</button>
+            <button onClick={() => setCookiesAccepted(!cookiesAccepted)}>I like Cookies</button>
             </div>
          
         </div>
+        )
+ 
      );
 }
  
-export default CookiePopUp;
+export default CookiePopUp; 
