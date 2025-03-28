887e7cacf1898ca0b1e2ff5f85b8d919c79b7995
matilde <mabr0005@stud.kea.dk>
2025-03-28 14:53:15 +0100

    color states

--- iwatch-figma-design/src/components/Gallery.jsx
+++ iwatch-figma-design/src/components/Gallery.jsx
@@ -1,31 +1,56 @@
+"use client";
+
 import { TbLineDotted } from "react-icons/tb";
 import Image from "next/image";
 import navyWatch from "@/img/navy.png"
 import mintWatch from "@/img/mint.png"
 import oceanWatch from "@/img/ocean.png"
+import { useState } from "react";
 
 const Gallery = () => {
+    const [activeImg, setActiveImg] = useState(navyWatch);
     return (
     <div className="gallery-container">
 <div className="active-img">
-    <Image src={navyWatch} alt="Navy Watch"/>
+    <Image src={activeImg} alt="Watch"/>
 </div>
 <div className="color-selector">
-    <div className="active" style={{backgroundColor: "#434558"}}></div>
+    <div
+    className={activeImg === navyWatch ? "active" : ""}
+    onClick={() => setActiveImg(navyWatch)}
+    style={{backgroundColor: "#434558"}}></div>
     <TbLineDotted style={{ transform: "rotate(90deg)" }} />
-    <div style={{backgroundColor: "#6ADDCC"}}></div>
+    <div
+    className={activeImg === mintWatch ? "active" : ""}
+    onClick={() => setActiveImg(mintWatch)}
+     style={{backgroundColor: "#6ADDCC"}}></div>
     <TbLineDotted style={{ transform: "rotate(90deg)" }} />
-    <div style={{backgroundColor: "#def3f6"}}></div>
+    <div
+    className={activeImg === oceanWatch ? "active" : ""}
+    onClick={() => setActiveImg(oceanWatch)}
+    style={{backgroundColor: "#def3f6"}}></div>
 </div>
 <div className="watch-gallery">
-    <figure>
-    <Image src={navyWatch} alt="Navy Watch"/>
+    <figure
+    onClick={() => setActiveImg(navyWatch)}>
+        <Image
+        src={navyWatch}
+        alt="Navy Watch"
+        />
     </figure>
-    <figure>
-    <Image src={mintWatch} alt="Mint Watch"/>
+    <figure
+    onClick={() => setActiveImg(mintWatch)}>
+        <Image
+        src={mintWatch}
+        alt="Mint Watch"
+        />
     </figure>
-    <figure>
-    <Image src={oceanWatch} alt="Ocean Watch"/>
+    <figure
+    onClick={() => setActiveImg(oceanWatch)}>
+        <Image
+        src={oceanWatch}
+        alt="Ocean Watch"
+        />
     </figure>
     
 </div>
