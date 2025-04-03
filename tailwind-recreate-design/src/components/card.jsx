c26831814fad23a105cf1f95e46def0746e1065d
matilde <mabr0005@stud.kea.dk>
2025-04-03 12:30:15 +0200

    recreate init

---
+++ tailwind-recreate-design/src/components/card.jsx
@@ -0,0 +1,11 @@
+import Styles from "./card.module.css"
+
+const Card = () => {
+    return ( 
+        <div className={Styles.Card}>
+<h2>Hej</h2>
+        </div>
+     );
+}
+ 
+export default Card;
